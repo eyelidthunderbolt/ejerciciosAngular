cc435edf3f2a45b77f4a3c269c60e95f9ffef612
Mario Acuña <m.acuna@salesianospizarrales.educamos.com>
2023-11-13 11:26:39 +0100

    Crear Rama ejercicio3

---
+++ src/app/ejercicio3-repaso/ejercicio3-repaso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio3-repaso',
+  templateUrl: './ejercicio3-repaso.component.html',
+  styleUrls: ['./ejercicio3-repaso.component.css']
+})
+export class Ejercicio3RepasoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
