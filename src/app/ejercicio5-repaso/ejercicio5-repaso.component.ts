5ed70db63e074ac7d1314d1bf1100ba7afd40d89
Mario Acuña <m.acuna@salesianospizarrales.educamos.com>
2023-11-14 18:03:25 +0100

    Crear Ejercicio5Repaso

---
+++ src/app/ejercicio5-repaso/ejercicio5-repaso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio5-repaso',
+  templateUrl: './ejercicio5-repaso.component.html',
+  styleUrls: ['./ejercicio5-repaso.component.css']
+})
+export class Ejercicio5RepasoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
